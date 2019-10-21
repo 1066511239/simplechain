e6c7d4b5ecb9c8c064d1a2221d4668c9ead5c9a6
walker <chenwei1204@outlook.com>
2019-10-21 11:36:43 +0800

    add level db for saving hashData and txID

---
+++ cmd/dummytx/db/db.go
@@ -0,0 +1,155 @@
+package db
+
+import (
+	"git.dev.tencent.com/baoquan2017/ccc-resonance/log"
+	"github.com/simplechain-org/simplechain/common"
+	"sync"
+
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/errors"
+	"github.com/syndtr/goleveldb/leveldb/filter"
+	"github.com/syndtr/goleveldb/leveldb/iterator"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
+)
+
+type IdHash struct {
+	Id   common.Hash `json:"id"`
+	Hash common.Hash `json:"hash"`
+}
+
+type LDBDatabase struct {
+	fn string      // filename for reporting
+	db *leveldb.DB // LevelDB instance
+
+	quitLock sync.Mutex      // Mutex protecting the quit channel access
+	quitChan chan chan error // Quit channel to stop the metrics collection before closing the database
+
+	log log.Logger // Contextual logger tracking the database path
+}
+
+// NewLDBDatabase returns a LevelDB wrapped object.
+func NewLDBDatabase(file string, cache int, handles int) (*LDBDatabase, error) {
+	logger := log.New("database")
+	logger.SetHandler(log.DiscardHandler())
+
+	// Ensure we have some minimal caching and file guarantees
+	if cache < 16 {
+		cache = 16
+	}
+	if handles < 16 {
+		handles = 16
+	}
+	logger.Info("Allocated cache and file handles cache %d, handles %d", cache, handles)
+
+	// Open the db and recover any potential corruptions
+	ldb, err := leveldb.OpenFile(file, &opt.Options{
+		OpenFilesCacheCapacity: handles,
+		BlockCacheCapacity:     cache / 2 * opt.MiB,
+		WriteBuffer:            cache / 4 * opt.MiB, // Two of these are used internally
+		Filter:                 filter.NewBloomFilter(10),
+	})
+	if _, corrupted := err.(*errors.ErrCorrupted); corrupted {
+		ldb, err = leveldb.RecoverFile(file, nil)
+	}
+	// (Re)check for errors and abort if opening of the db failed
+	if err != nil {
+		return nil, err
+	}
+	return &LDBDatabase{
+		fn:  file,
+		db:  ldb,
+		log: logger,
+	}, nil
+}
+
+// Path returns the path to the database directory.
+func (ldb *LDBDatabase) Path() string {
+	return ldb.fn
+}
+
+// Put puts the given key / value to the queue
+func (ldb *LDBDatabase) Put(key []byte, value []byte) error {
+	return ldb.db.Put(key, value, nil)
+}
+
+func (ldb *LDBDatabase) Has(key []byte) (bool, error) {
+	return ldb.db.Has(key, nil)
+}
+
+// Get returns the given key if it's present.
+func (ldb *LDBDatabase) Get(key []byte) ([]byte, error) {
+	dat, err := ldb.db.Get(key, nil)
+	if err != nil {
+		return nil, err
+	}
+	return dat, nil
+}
+
+// Delete deletes the key from the queue and database
+func (ldb *LDBDatabase) Delete(key []byte) error {
+	return ldb.db.Delete(key, nil)
+}
+
+func (ldb *LDBDatabase) NewIterator() iterator.Iterator {
+	return ldb.db.NewIterator(nil, nil)
+}
+
+// NewIteratorWithPrefix returns a iterator to iterate over subset of database content with a particular prefix.
+func (ldb *LDBDatabase) NewIteratorWithPrefix(prefix []byte) iterator.Iterator {
+	return ldb.db.NewIterator(util.BytesPrefix(prefix), nil)
+}
+
+func (ldb *LDBDatabase) Close() {
+	// Stop the metrics collection to avoid internal database races
+	ldb.quitLock.Lock()
+	defer ldb.quitLock.Unlock()
+
+	if ldb.quitChan != nil {
+		errc := make(chan error)
+		ldb.quitChan <- errc
+		if err := <-errc; err != nil {
+			ldb.log.Error("Metrics collection failed", "err", err)
+		}
+		ldb.quitChan = nil
+	}
+	err := ldb.db.Close()
+	if err == nil {
+		ldb.log.Info("Database closed")
+	} else {
+		ldb.log.Error("Failed to close database", "err", err)
+	}
+}
+
+func (ldb *LDBDatabase) LDB() *leveldb.DB {
+	return ldb.db
+}
+
+func (ldb *LDBDatabase) InsertHash(hash, id common.Hash) error {
+	return ldb.Put(hash.Bytes(), id.Bytes())
+}
+
+func (ldb *LDBDatabase) GetHashId(hash common.Hash) (common.Hash, error) {
+	data, err := ldb.Get(hash.Bytes())
+	if err != nil {
+		return common.Hash{}, err
+	}
+	return common.BytesToHash(data), nil
+}
+
+//func (ldb *LDBDatabase) GetAllRawTrades() (trades protocol.BtcTrades, err error) {
+//	itr := ldb.NewIterator()
+//	itr.First()
+//
+//	for len(itr.Value()) != 0 {
+//		var trade protocol.BtcTrade
+//		if err := rlp.DecodeBytes(itr.Value(), &trade); err != nil {
+//			return nil, err
+//		}
+//		trades = append(trades, trade)
+//		itr.Next()
+//	}
+//
+//	sort.Sort(trades)
+//	return trades, nil
+//}
--- cmd/dummytx/dummytx.go
+++ cmd/dummytx/dummytx.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"flag"
+	"fmt"
 	"log"
 	"math/big"
 	"math/rand"
@@ -14,6 +15,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/simplechain-org/simplechain/cmd/dummytx/db"
 	"github.com/simplechain-org/simplechain/common"
 	"github.com/simplechain-org/simplechain/core/types"
 	"github.com/simplechain-org/simplechain/crypto"
@@ -36,16 +38,57 @@ var (
 		"5fc3281bd2894b8418a895897fbc7fe204099ae2f99f930da195d393976d1bbc",
 		"ac817b6310c8ce7a2fd6783a663e4d47f0ab1fb3ae2fba5f85f07bdca5a36e90",
 		"94dbb9085d79578046c4b10a0a7baefead738371ac763ee6ed7d727d348a2509",
-		"bfcca7c164821bc6162657da43e816a78b5993ecaa8174a56a90788a83707749",
-		"31f6f1e0b357c6c4b626e96b84a09289ccb0dd565a4d87831f7b5e49384fbe14",
-		"50c5633909248612d848d4c8d353b57eb0802b8ae1bcea5d89b5cfe0134efa52",
-		"1c502ae118d54d9b9f4c6bb47e9aff53e705c2eb2464c3403be445dc975d5f32",
-		"b1c6d3a853d30624a771d0c29161ad32c7179d141d7d4b5e6534fc8b2514d8f6",
-		"9284053f3a224697240b67a2450b8fe6bcaa07c61ab5f134f2dba41d76b506f4",
-		"024bce5b3cd68652749937e9028e57ecac49ee3342fef15c76887d9bf144e51a",
-		"d30976fcd0846a346c8de3a327170a2c96fb8a00972b03a84eedbfbc18b646d8",
-		"2f06cf29ef2f787ac1a7b18cc89db27387dbb9c2a868914c5c506bee33e506f0",
-		"6e5d936f2578d7805d1aa70f3e6d82492dfc5af838c2bbbe50fa2a343aab43e8",
+		//
+		//"bfcca7c164821bc6162657da43e816a78b5993ecaa8174a56a90788a83707749",
+		//"31f6f1e0b357c6c4b626e96b84a09289ccb0dd565a4d87831f7b5e49384fbe14",
+		//"50c5633909248612d848d4c8d353b57eb0802b8ae1bcea5d89b5cfe0134efa52",
+		//"1c502ae118d54d9b9f4c6bb47e9aff53e705c2eb2464c3403be445dc975d5f32",
+		//"b1c6d3a853d30624a771d0c29161ad32c7179d141d7d4b5e6534fc8b2514d8f6",
+		//"9284053f3a224697240b67a2450b8fe6bcaa07c61ab5f134f2dba41d76b506f4",
+		//"024bce5b3cd68652749937e9028e57ecac49ee3342fef15c76887d9bf144e51a",
+		//"d30976fcd0846a346c8de3a327170a2c96fb8a00972b03a84eedbfbc18b646d8",
+		//"2f06cf29ef2f787ac1a7b18cc89db27387dbb9c2a868914c5c506bee33e506f0",
+		//"6e5d936f2578d7805d1aa70f3e6d82492dfc5af838c2bbbe50fa2a343aab43e8",
+		//"a0a3d33afe86bd99827604caade4a2418b5e1c0f4213626763c8a4904ada5552",
+		//"7c7f9280fa44a190f4bafa8f75f1a272f18a8766f9fab4e01c68f190db6ff943",
+		//"50af8f1078fe4bd4f4a16fcddcc19a73fc15ef74edae4df7512fe72dc44e8636",
+		//"c7faed5ef7fecd406ad9b362d09b0b7d5abccd4f4e308c572417c8340e159f6e",
+		//"66e0318c674ccfe3b6ed7c9177e8a73a6d6e3377507bc85a31ae10539b2f760e",
+		//"359137e052174a1729181798e4ffc53be5232153a82851ca0d4a67f891cf7cfd",
+		//"58db0bb015886e544e742fdfca3d50c5347a8a79f98b150adea9f4ac6a89ce5d",
+		//"9c277eed4578e41880c830363a07ab27df757b91b85a10a2446bc0ebcf8141ad",
+		//"724bf5ebe6058b9be382c42d3c906987dbccd557b36a3b7f7416f25efdffe30e",
+		//"81fabd0982b836ca7f853a44900d150593243c7105c6c87489f4436fbe047329",
+		//"ce542dc71c9b0316a7644e4abf5987b8afa9ba49300931ab65d1d3fd60267cc9",
+		//"f51eb310a91fd28d67f9866d616a9c4d8cefafbfde64afbc06e0505550b606e2",
+		//"e5ac84fca4a2219e05f577084c1361cf25c2cf45ba07eee135bf4a7f61c3c9c5",
+		//"c99d1c92b1e8a4111d7a0766eb2b05ce827d9a75481de3159ffc40fde585defb",
+		//"3ce36df955dbab881213b4aac5412a83f75ca012f257f6caaf73566101974f36",
+		//"ace0bbf5540b9074168d77ccece1779a73511b23c9864f1e0d000c4e18a69c42",
+		//"a6ecd58fc6c1ffb03e54f21218c713b6aef9dbb61212e1d8b8de521846e99c71",
+		//"ad36f5efd748dc7895366d028badc5bfcac3355f3d1151760a50e7e13b4502be",
+		//"73ca1ecefd17f0aaccd665ab1d2765fe44f272eb99744cf8128fd21547d7a9a9",
+		//"14e7b973f57a7707ac10a6e26892f5b3041cb8f735d35edf47f7d23d067baeb5",
+		//"2e2968d7a8e6482654ace269e333b2d0dbabc4e594b8fc594ba3be636f264c5d",
+		//"d5d5d6ec9c08a09e2d45d539e724bb2181c8c25586f0ab4fe08285b943cf061b",
+		//"0a421bb214d61aa943328993d785ccc15ad049cc31fde87bc06232547046c33b",
+		//"3b3948e44e46d66587df14cf101570242bc24a568063ec7b046ffc8ed93bc2cd",
+		//"4f9f72b9c4a688e3a6c4268627b6188704071d4efc919b23a45ad13ce9a56b17",
+		//"7111f9be4a79363bbc242ab83a9d73b3fe8f072443463acf62c882aeb6633aaf",
+		//"4bd077b5d373a40deabe0beed7f7eaf78a43fb582117af273e367f857624cdf7",
+		//"c299e149fc6445772f30e7a3e183dbff2369cbe10a5f7bc685aaf06ec7fab8b7",
+		//"faf00b3ee6162e6f126e59ab823f9fb847c7a821ae9f850a296cde4067ba91f0",
+		//"a29199156d5e404ce1b8468cd5fbc8859251a9cbaedf6877d343cf9916adfac0",
+		//"60091525639cd74720f2095d72196b2e5b222c7b7930fe4d2b1225cbd097488e",
+		//"23f0b7099b105abbb3ac694068752b4210cbadb89397b29a66d983ee4ffda9b5",
+		//"e0a48bd0478379c420e1c3c6cab0cc224dc6cdaeb488a10d87852e2d0d33875e",
+		//"9b0ca2cf966db34535d160392f8f8773f78b3a9f58b0c8b5ad376eff9db7b5e1",
+		//"e04bc7cc3d04f00a5dc96a0843e4ad63d0bc5d25977768911b1e5bf990607d94",
+		//"2f9e2da9d4fef8f79f9fafc5e173c7ab175152137eec8ec9d3390e152f04c6e8",
+		//"a552515da13ea1f8810d4a38c34dbf057f227954d307b1ef0b84d548f755703e",
+		//"a742b92fce506bb28a1c33a3dce2eadf529062e37ad27997492ab9dac7ec66fb",
+		//"cb7443ba9adec3554aaa4b9de53bd679b7351ad54915409550fbd9f31f2da876",
+		//"4a904d7d5dc78bcd7b582cf5ce7b03b6dce92772c353b2936564a6924bbe5a57",
 	}
 )
 
@@ -60,8 +103,35 @@ func main() {
 	startBlock := flag.Int("startBlock", 10, "calculate tps start from this block")
 
 	url := flag.String("url", "ws://localhost:8546", "websocket url")
+
+	PrintDB := flag.Bool("printDB", false, "get hashID from DB")
+	getTxIDByHashData := flag.String("hashID", "", "get tx ID by hash data")
 	flag.Parse()
-	//fmt.Println(*sendTx, *startBlock, *url, *MonitorTx)
+
+	hashDb, err := db.NewLDBDatabase("./IdHash", 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *PrintDB {
+		itr := hashDb.NewIterator()
+		itr.First()
+
+		for itr.Valid() {
+			fmt.Println("hash data: ", common.BytesToHash(itr.Key()).String(), "tx ID: ", common.BytesToHash(itr.Value()).String())
+			itr.Next()
+		}
+		itr.Release()
+		return
+	}
+
+	if len(*getTxIDByHashData) == 66 {
+		txID, err := hashDb.GetHashId(common.HexToHash(*getTxIDByHashData))
+		if err != nil {
+			log.Printf("getHashId failed,hash:%s, err: %s", *getTxIDByHashData, err)
+		}
+		log.Println("txID: ", txID.String())
+		return
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	client, err := ethclient.Dial(*url)
@@ -90,7 +160,7 @@ func main() {
 			fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 			log.Printf("sender:%d %s\n", i, fromAddress.String())
 
-			go dummyTx(ctx, client, i, fromAddress)
+			go dummyTx(ctx, client, i, fromAddress, hashDb)
 		}
 	}
 	if *MonitorTx {
@@ -108,7 +178,7 @@ func main() {
 	log.Println("dummy transaction exit")
 }
 
-func dummyTx(ctx context.Context, client *ethclient.Client, index int, fromAddr common.Address) {
+func dummyTx(ctx context.Context, client *ethclient.Client, index int, fromAddr common.Address, hashDb *db.LDBDatabase) {
 	nonce, err := client.PendingNonceAt(ctx, fromAddr)
 	if err != nil {
 		log.Fatalf(errPrefix+" get new nonce: %v", err)
@@ -152,7 +222,7 @@ func dummyTx(ctx context.Context, client *ethclient.Client, index int, fromAddr 
 		default:
 			//build,sign,send transaction
 			_, _ = rand.Read(data[20:]) //read random 64 bytes
-			dummy(ctx, nonce, toAddress, value, gasLimit, gasPrice, data[:], client, fromAddr)
+			dummy(ctx, nonce, toAddress, value, gasLimit, gasPrice, data[:], client, fromAddr, hashDb)
 
 			switch {
 			case nonce%20000 == 0:
@@ -169,11 +239,16 @@ func dummyTx(ctx context.Context, client *ethclient.Client, index int, fromAddr 
 	}
 }
 
-func dummy(ctx context.Context, nonce uint64, toAddress common.Address, value *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte, client *ethclient.Client, fromAddress common.Address) {
+func dummy(ctx context.Context, nonce uint64, toAddress common.Address, value *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte, client *ethclient.Client, fromAddress common.Address, hashDb *db.LDBDatabase) {
 	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
 	err := client.SendTransaction(ctx, tx)
 	if err != nil {
 		log.Printf(warnPrefix+" send tx: %v", err)
+	}
+	fmt.Println(common.BytesToHash(tx.Data()[20:]).String(), tx.Hash().String())
+	err = hashDb.InsertHash(common.BytesToHash(tx.Data()[20:]), tx.Hash())
+	if err != nil {
+		fmt.Printf("InsertHash failed: hash: %s,txId:%s\n", common.BytesToHash(tx.Data()[20:]).String(), tx.Hash().String())
 	}
 
 }
